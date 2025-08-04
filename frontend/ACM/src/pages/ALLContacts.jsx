cf600f5b849797f60ff3ebb7fed3917040ae1f4d
rimaypatil <rimaypatil2612@gmail.com>
2025-08-04 13:25:55 +0530

    check

--- frontend/ACM/src/pages/ALLContacts.jsx
+++ frontend/ACM/src/pages/ALLContacts.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import './AllContacts.css';
+import './ALLContacts.css';
 
 function AllContacts() {
   const [contacts, setContacts] = useState([]);
